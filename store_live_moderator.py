6df3a6f387331b85fa3d3cfe88fdfb0ed050cff1
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-02-12 13:45:05 +0700

    Remove print moderator data

--- store_live_moderator.py
+++ store_live_moderator.py
@@ -3,11 +3,6 @@ import uuid
 
 
 def store_live_moderator(channel_detail, live_moderator_data):
-    print('Moderator %s : %s (%s)' % (
-        channel_detail['snippet']['title'],
-        live_moderator_data['channelId'],
-        live_moderator_data['displayName']
-    ))
     moderator = LiveModerator()
     moderator.uuid = uuid.uuid4()
     moderator.owner_channel_id = channel_detail['id']
