385d5b65a33f4ba1bf40876578037742f7a11155
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-02-12 16:52:07 +0700

    Clear live moderators list before insert the new list

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from googleapiclient.errors import HttpError
 import fetch_user_credential as auth
 from fetch_channel_detail import fetch_channel_detail
+from clear_live_moderators import clear_live_moderators
 from fetch_last_broadcast import fetch_last_broadcast
 from iterate_live_moderator import iterate_live_moderator
 from store_live_moderator import store_live_moderator
@@ -16,6 +17,7 @@ if __name__ == '__main__':
             channel_detail = channel_details[0]
         else:
             raise ChannelDetailNotFound
+        clear_live_moderators(channel_detail['id'])
         last_broadcasts = fetch_last_broadcast(youtube_api)
         if len(last_broadcasts) > 0:
             live_chat_id = last_broadcasts[0]['snippet']['liveChatId']
